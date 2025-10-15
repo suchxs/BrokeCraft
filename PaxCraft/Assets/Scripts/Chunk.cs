14447a011e791f5e56770c1e43b5393e63e17049
watsonjph <corsegajoseph@gmail.com>
2025-10-15 20:22:21 +0800

    REFACTOR: Switched to JaggedArray

--- PaxCraft/Assets/Scripts/Chunk.cs
+++ PaxCraft/Assets/Scripts/Chunk.cs
@@ -14,22 +14,28 @@ public class Chunk : MonoBehaviour
         int vertexIndex = 0;
         List<Vector3> vertices = new List<Vector3> ();
         List<int> triangles = new List<int> ();
+        List<Vector2> uvs = new List<Vector2> ();
 
-        for (int i = 0; i < 6; i++)
+
+        for (int face = 0; face < 6; face++)
         {
-            int triangleIndex = VoxelData.voxelTris[0, i];
-            vertices.Add (VoxelData.voxelVerts[triangleIndex]);
-            triangles.Add (vertexIndex);
-
-            vertexIndex++;
-
+            for (int i = 0; i < 6; i++)
+            {
+                int triIndex = VoxelData.voxelTris[face][i];
+                vertices.Add(VoxelData.voxelVerts[triIndex]);
+                triangles.Add(vertexIndex);
+                uvs.Add(VoxelData.voxelUvs[triIndex % 4]); // map 0-3 repeatedly
+                vertexIndex++;
+            }
         }
 
 
         Mesh mesh = new Mesh ();
         mesh.vertices = vertices.ToArray ();
         mesh.triangles = triangles.ToArray ();
+        mesh.uv = uvs.ToArray ();
 
         mesh.RecalculateNormals ();
+        meshFilter.mesh = mesh;
     }
 }
--- PaxCraft/Assets/Scripts/VoxelData.cs
+++ PaxCraft/Assets/Scripts/VoxelData.cs
@@ -15,11 +15,27 @@ public static class VoxelData
         new Vector3(0.0f, 1.0f, 1.0f),
     };
 
+    // data from Mojang themselves lol
 
-    public static readonly int[,] voxelTris = new int[1, 6]
+    // 6 vertices (inices) per face
+
+    public static readonly int[][] voxelTris = new int[][]
     {
-        {3, 7, 2, 2, 7, 6 } // Top Face, Based on voxelVerts coords
+    new int[] {0, 3, 1, 1, 3, 2}, // Back Face
+    new int[] {5, 6, 4, 4, 6, 7}, // Front Face
+    new int[] {3, 7, 2, 2, 7, 6}, // Top Face
+    new int[] {1, 5, 0, 0, 5, 4}, // Bottom Face
+    new int[] {4, 7, 0, 0, 7, 3}, // Left Face
+    new int[] {1, 2, 5, 5, 2, 6}  // Right Face
+    };
 
-
+    public static readonly Vector2[] voxelUvs = new Vector2[6]
+    {
+        new Vector2 (0.0f, 0.0f),
+        new Vector2 (0.0f, 1.0f),
+        new Vector2 (1.0f, 0.0f),
+        new Vector2 (1.0f, 0.0f),
+        new Vector2 (0.0f, 1.0f),
+        new Vector2 (1.0f, 1.0f)
     };
 }
